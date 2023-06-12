76b92c9f43ce5c91da7337a04bf2839d9d8e7132
busybox11 <busybox11th@gmail.com>
2023-06-12 09:12:13 +0000

    app: dotenv for mysql

---
+++ views/mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../config.php';
+
+// Create connection
+$conn = new mysqli($mysql_config['host'], $mysql_config['user'], $mysql_config['password'], $mysql_config['database'], $mysql_config['port']);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
