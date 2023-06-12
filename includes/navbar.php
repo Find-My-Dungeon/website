7037b4a66915ca518debb2328720d8612a26dcdc
busybox11 <busybox11th@gmail.com>
2023-06-12 07:09:13 +0000

    login: Initial commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,44 +1,48 @@
-<header class="flex flex-row items-center justify-between px-6 py-2 bg-red-800 text-white">
+<header class="h-16 flex flex-row items-center justify-between px-6 py-2 bg-red-800 text-white">
     <div class="logo">
         <div class="flex h-10 w-10 bg-white text-black">
             <span class="m-auto font-bold">FMD</span>
         </div>
     </div>
 
-    <div x-data="{ tab: '' }" class="flex flex-row gap-4 md:gap-12 items-center">
-        <div @mouseover="tab = 'sheets'" @mouseout="tab = ''" class="relative py-3">
-            <h2 class="menu-dropdown">Mes fiches</h2>
-            <div x-show="tab == 'sheets'" class="absolute top-10 left-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
-                <a href="#">Personnages</a>
-                <a href="#">Aventure</a>
+    <?php if ($page_title != "Login") { ?>
+        <div x-data="{ tab: '' }" class="flex flex-row gap-4 md:gap-12 items-center">
+            <div @mouseover="tab = 'sheets'" @mouseout="tab = ''" class="relative py-3">
+                <h2 class="menu-dropdown">Mes fiches</h2>
+                <div x-show="tab == 'sheets'" class="absolute top-10 left-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
+                    <a href="#">Personnages</a>
+                    <a href="#">Aventure</a>
+                </div>
             </div>
-        </div>
-        <h2 class="menu-link"><a href="#">Rechercher</a></h2>
-    </div>
-
-    <div x-data="{ tab: '' }" class="flex flex-row gap-2">
-        <div @mouseover="tab = 'user'" @mouseout="tab = ''" class="relative py-3 px-4">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-circle-2">
-                <path d="M18 20a6 6 0 0 0-12 0" />
-                <circle cx="12" cy="10" r="4" />
-                <circle cx="12" cy="12" r="10" />
-            </svg>
-
-            <div x-show="tab == 'user'" class="absolute top-12 right-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
-                <a href="#">Mon profil</a>
-                <a href="#">Déconnexion</a>
-            </div>
+            <h2 class="menu-link"><a href="#">Rechercher</a></h2>
         </div>
 
-        <div @mouseover="tab = 'notifications'" @mouseout="tab = ''" class="relative py-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell">
-                <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-                <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-            </svg>
+        <div x-data="{ tab: '' }" class="flex flex-row gap-2">
+            <div @mouseover="tab = 'user'" @mouseout="tab = ''" class="relative py-3 px-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-circle-2">
+                    <path d="M18 20a6 6 0 0 0-12 0" />
+                    <circle cx="12" cy="10" r="4" />
+                    <circle cx="12" cy="12" r="10" />
+                </svg>
 
-            <div x-show="tab == 'notifications'" class="absolute top-12 right-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
-                <span class="italic text-zinc-700 flex-shrink-0">No notifications.</span>
+                <div x-show="tab == 'user'" class="absolute top-12 right-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
+                    <a href="#">Mon profil</a>
+                    <a href="#">Déconnexion</a>
+                </div>
+            </div>
+
+            <div @mouseover="tab = 'notifications'" @mouseout="tab = ''" class="relative py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell">
+                    <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+                    <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+                </svg>
+
+                <div x-show="tab == 'notifications'" class="absolute top-12 right-0 flex flex-col gap-2 px-6 py-4 bg-gray-100 text-black">
+                    <span class="italic text-zinc-700 flex-shrink-0">No notifications.</span>
+                </div>
             </div>
         </div>
-    </div>
+    <?php } else { ?>
+        <h2 class="text-xl font-bold mx-auto">FindMyDungeon</h2>
+    <?php } ?>
 </header>
---
+++ login.php
@@ -0,0 +1,56 @@
+<?php
+
+$page_title = "Login";
+
+include_once 'includes/header.php';
+?>
+<body>
+    <?php include_once 'includes/navbar.php'; ?>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 py-4 md:py-6 lg:py-8 w-11/12 max-w-screen-xl mx-auto">
+        <div class="flex flex-col gap-4 px-6 py-3 rounded-2xl border-4 border-blue-300 bg-blue-100 mb-auto">
+            <h2 class="text-2xl font-bold">Se connecter</h2>
+
+            <form action="login.php" method="post">
+                <div class="flex flex-col gap-4">
+                    <div class="flex flex-col gap-1">
+                        <label for="email_login">Adresse email</label>
+                        <input class="px-3 py-1 rounded-2xl border-2 border-blue-700 transition focus:ring-2 focus:ring-blue-800 border-blue-800 outline-none" type="email" name="email_login" id="email_login" placeholder="example@gmail.com" required>
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <label for="password_login">Mot de passe</label>
+                        <input class="px-3 py-1 rounded-2xl border-2 border-blue-700 transition focus:ring-2 focus:ring-blue-800 border-blue-800 outline-none" type="password" name="password_login" id="password_login" placeholder="" required>
+                    </div>
+
+                    <input type="submit" value="Se connecter" class="btn btn-blue my-1 py-2 w-2/3 mx-auto bg-purple-500 text-white rounded-3xl">
+                </div>
+            </form>
+        </div>
+
+        <div class="flex flex-col gap-4 px-6 py-3 rounded-2xl border-4 border-purple-300 bg-purple-100 lg:col-span-2">
+            <h2 class="text-2xl font-bold">S'inscrire</h2>
+
+            <form action="login.php" method="post">
+                <div class="flex flex-col gap-4">
+                    <div class="flex flex-col gap-1">
+                        <label for="username_register">Nom d'utilisateur</label>
+                        <input class="px-3 py-1 rounded-2xl border-2 border-blue-700 transition focus:ring-2 focus:ring-blue-800 border-blue-800 outline-none" type="text" name="username_register" id="username_register" placeholder="PHiBi_is_c00l" required>
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <label for="email_register">Adresse email</label>
+                        <input class="px-3 py-1 rounded-2xl border-2 border-blue-700 transition focus:ring-2 focus:ring-blue-800 border-blue-800 outline-none" type="email" name="email_register" id="email_register" placeholder="example@gmail.com" required>
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <label for="password_register">Mot de passe</label>
+                        <input class="px-3 py-1 rounded-2xl border-2 border-blue-700 transition focus:ring-2 focus:ring-blue-800 border-blue-800 outline-none" type="password" name="password_register" id="password_register" placeholder="" required>
+                    </div>
+
+                    <input type="submit" value="S'inscrire" class="btn btn-blue my-1 py-2 w-2/3 lg:w-1/2 mx-auto bg-purple-500 text-white rounded-3xl">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php include_once 'includes/footer.php'; ?>
+</body>
+</html>
