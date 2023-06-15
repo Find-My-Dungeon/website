580bd8e75d2e2af2b2483b772dbd3fc8e07044c6
Thomas GIRESSE <thomas.giresse@viacesi.fr>
2023-06-15 12:07:49 +0000

    modif de character  vers adventurer

--- data/adventures.php
+++ data/adventures.php
@@ -16,7 +16,7 @@ function adventure_card($adventure) {
     <div class="px-4 py-3 rounded-xl border-2 border-purple-300 bg-purple-100 w-full md:basis-1/2 lg:basis-1/3">
         <h4 class="text-lg font-bold"><?php echo $adventure["title"]; ?></h4>
         <h4 class="text-lg font-bold">Genre : <?php echo $adventure["genre"]; ?></h4>
-        <p><?php echo $adventure["number_character"]; ?> / <?php echo $adventure["max_number_character"]; ?> aventuriers</p>
+        <p><?php echo $adventure["number_adventurer"]; ?> / <?php echo $adventure["max_number_adventurer"]; ?> aventuriers</p>
         <p><?php echo $adventure["resume_story"]; ?></p>
     </div>
     <?php
