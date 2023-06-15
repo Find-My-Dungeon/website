67f345d3d1c79e1f654ae15577d1a07f56d05dba
Thomas GIRESSE <thomas.giresse@viacesi.fr>
2023-06-15 13:39:17 +0000

    test formulaire

--- views/newCharacter.php
+++ views/newCharacter.php
@@ -1,8 +1,9 @@
 <?php
 $page_title = "Nouvelle fiche utilisateur";
 include_once '../includes/header.php';
-?>
-<body>
+var_dump($_SESSION);
+die("toto");    
+?><body>
     <?php include_once '../includes/navbar.php'; ?>
 
     <main class="flex flex-col py-4 md:py-6 lg:py-8 w-11/12 max-w-screen-xl mx-auto">
@@ -11,15 +12,15 @@ include_once '../includes/header.php';
             
             <div class="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-6 w-full pb-2">
                 <div class="flex flex-col gap-2">
-                    <label for="name">Nom du personnage</label>
+                    <label for="name_adventurer">Nom du personnage</label>
                     <input class="outline-none rounded-lg px-2 py-1 border-2 border-blue-500 transition hover:border-blue-600 focus:border-blue-700 focus:ring-2 focus:ring-blue-700" type="text" name="name" id="name" placeholder="ex. PhiBi" required />
                 </div>
                 <div class="flex flex-col gap-2">
-                    <label for="class">Classe</label>
+                    <label for="classe">Classe</label>
                     <input class="outline-none rounded-lg px-2 py-1 border-2 border-blue-500 transition hover:border-blue-600 focus:border-blue-700 focus:ring-2 focus:ring-blue-700" type="text" name="class" id="class" placeholder="ex. UserFactory" required />
                 </div>
                 <div class="flex flex-col gap-2">
-                    <label for="species">Espèce</label>
+                    <label for="race">Espèce</label>
                     <input class="outline-none rounded-lg px-2 py-1 border-2 border-blue-500 transition hover:border-blue-600 focus:border-blue-700 focus:ring-2 focus:ring-blue-700" type="text" name="species" id="species" placeholder="ex. public static void" required />
                 </div>
                 <div class="flex flex-col gap-2">
@@ -31,7 +32,7 @@ include_once '../includes/header.php';
                     <input class="outline-none rounded-lg px-2 py-1 border-2 border-blue-500 transition hover:border-blue-600 focus:border-blue-700 focus:ring-2 focus:ring-blue-700" type="text" name="avatar" id="avatar" placeholder="URL de l'avatar du personnage" required />
                 </div>
                 <div class="flex flex-col gap-2">
-                    <label for="bio">Histoire et origine</label>
+                    <label for="resume_adventurer">Histoire et origine</label>
                     <textarea class="outline-none rounded-lg px-2 py-1 border-2 border-blue-500 transition hover:border-blue-600 focus:border-blue-700 focus:ring-2 focus:ring-blue-700" name="bio" id="bio" placeholder="" required></textarea>
                 </div>
                 <div class="flex flex-col gap-2">
@@ -43,6 +44,40 @@ include_once '../includes/header.php';
             <button class="bg-purple-500 hover:bg-purple-600 focus:bg-purple-700 active:bg-purple-800 text-white font-bold py-2 px-4 rounded-lg w-1/2 mx-auto" type="submit">Créer</button>
         </form>
     </main>
+
+
+<?php 
+
+    
+
+    if (
+        !isset($_POST['name_adventurer'])
+        || !isset($_POST['classe'])
+        || !isset($_POST['race'])
+        || !isset($_POST['avatar'])
+        || !isset($_POST['resume_adventurer'])
+        )
+    {
+        echo 'Des informations obligatoires sont manquantes.';
+    }
+
+    $name_adventurer = htmlentities(trim($_POST['name_adventurer']));
+    $classe = $_POST['classe'];
+    $race = $_POST['race'];
+    $avatar = $_POST['avatar'];
+    $resume_adventurer = $_POST['resume_adventurer'];
+
+
+    require_once __DIR__ . '/../utils/mysql.php';
+
+    $sql = "INSERT INTO `adventurer` 
+        (`name_adventurer`, `classe`, `race`, `level`, `avatar`,  `resume_adventurer`, `id_user_adventurer`) 
+        VALUES (':name_adventurer', ':classe', ':race', ':level', ':avatar', ':resume_adventurer', ':id_user_adventurer')";   
+    $result = execute_sql($sql);
+
+    return $result;
+    
+?>
 
     <?php include_once '../includes/footer.php'; ?>
 </body>
