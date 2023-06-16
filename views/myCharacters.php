6770784b5e9b4ccc077f0ce705803ebc459c18b9
busybox11 <busybox11th@gmail.com>
2023-06-16 07:48:26 +0000

    myCharacters: Remove useless button

--- views/myCharacters.php
+++ views/myCharacters.php
@@ -28,10 +28,6 @@ $adventurers = get_user_adventurers($_SESSION["id"]);
                 <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><line x1="12" x2="12" y1="5" y2="19"/><line x1="5" x2="19" y1="12" y2="12"/></svg>
             </a>
         </div>
-        <button class="flex flex-row items-center gap-2 bg-purple-500 hover:bg-purple-600 focus:bg-purple-700 active:bg-purple-800 text-white font-bold py-2 px-4 rounded-lg" type="submit">
-            <span>Modifier</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-save"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>
-        </button>
     </main>
 
     <?php include_once '../includes/footer.php'; ?>
